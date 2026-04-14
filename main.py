e632398a5ac62b83c02f2ab7d4ae652b8c7266f9
erlyg <erlyginegor@gmail.com>
2026-04-14 22:58:44 +0300

    Сделал логотип сайта

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def index():
     return render_template('card_main.html', title='Smart Kitchen')
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
