95082ee54ba36686581da49a55aee5c51b025192
erlyg <erlyginegor@gmail.com>
2026-04-20 22:20:50 +0300

    ТЗ+Часть задания выполнена

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from data import db_session
+from data.models import Recipe, Ingredient
 
-TITLE = 'Smart Kitchen'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/recipes.db")
@@ -10,18 +10,21 @@ db_session.global_init("db/recipes.db")
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('card_main.html', title=TITLE)
+    return render_template('card_main.html')
 
 
 @app.route('/all_recipes')
 def all_recipes():
-    return render_template('all_recipes.html', title=TITLE)
+    db_sess = db_session.create_session()
+    recipes = db_sess.query(Recipe).all()
+    db_sess.close()
+    return render_template('all_recipes.html', recipes=recipes)
 
 
 @app.route('/search_page')
 def search_page():
-    return render_template('search_page.html', title=TITLE)
+    return render_template('search_page.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
