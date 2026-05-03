3b5717488119889d2a70b160d44f8002f8dc5d17
b <leshasadretdinov@gmail.com>
2026-05-03 14:28:06 +0300

    Сделал карточку кнопку добавления и список всех блюд

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def search_page():
 
 @app.route('/add_recipes')
 def add_recipes():
-    return render_template('add_recipes.html', title=TITLE)
+    return render_template('add_recipes.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
